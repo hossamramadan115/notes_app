c32d1cb2ce4a06b6f145ecb7f6531dfe18b0bcfe
hossam ramadan <hossamramadan115@gmail.com>
2025-02-12 16:04:37 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(),);
+  }
+}
