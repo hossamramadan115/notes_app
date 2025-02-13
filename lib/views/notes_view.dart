a7e6ac102559123c09ffa5636c556f686a7bb87c
hossam ramadan <hossamramadan115@gmail.com>
2025-02-13 12:41:28 +0200

    add show modal button sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_button_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,7 +9,16 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return AddNoteButtonSheet();
+              });
+        },
         child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
--- lib/views/widgets/note_list_view.dart
+++ lib/views/widgets/note_list_view.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_note-item.dart';
 
-
-
 class NoteListView extends StatelessWidget {
   const NoteListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child:  NoteItem(),
-      );
-    });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: NoteItem(),
+            );
+          }),
+    );
   }
 }
