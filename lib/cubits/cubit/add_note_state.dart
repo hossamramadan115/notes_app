59041afa573c1c73f66510921f1e64b371c50379
hossam ramadan <hossamramadan115@gmail.com>
2025-02-15 17:07:02 +0200

    create add note state

---
+++ lib/cubits/cubit/add_note_state.dart
@@ -0,0 +1,12 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+abstract class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSucess extends AddNoteState {}
+
+class AddNoteFailuer extends AddNoteState {}
